cd2792c9e4942e4ef8a3e21bb1be5f7eb06b060b
jimmyshinshin <b05902114@ntu.edu.tw>
2019-06-10 01:24:19 +0800

    yee

--- __init__.py
+++ __init__.py
@@ -2,17 +2,19 @@ import os
 
 from flask import Flask, redirect, url_for
 
-#from flask_sqlalchemy import SQLAlchemy
 	
 
 def create_app(test_config=None):
 	"""Create and configure an instance of the Flask application."""
 	app = Flask(__name__, instance_relative_config=True)
 
+	#warning: it is not recommended to set configuration directly
+	app.config["DEBUG"] = False
+	print(app.config["DEBUG"])
+
 	# apply the blueprints to the app
 	from csie_big_jj import auth
 
-	#db.init_app(app)
 
 	app.register_blueprint(auth.bp)
 
